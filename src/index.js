7e89be64d9c4ae900d235c3fe57fae4360d8d420
Salman <mhdsalman010@gmail.com>
2023-11-03 11:47:09 +0530

    fixed ugly scroll bar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import StarRating from "./StartRating";
 import "./index.css";
 import App from "./App";
-import Position from "./Position";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Position />
+    <App />
   </React.StrictMode>
 );
